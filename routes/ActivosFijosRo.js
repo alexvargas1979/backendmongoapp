47a878253b161486ddc9d149311fea9582e042d9
Carlos Daniel <c.ospina2001@pascualbravo.edu.co>
2020-05-28 16:13:29 -0500

    Mejorando el còdigo

--- routes/ActivosFijosRo.js
+++ routes/ActivosFijosRo.js
@@ -1,23 +1,23 @@
 import routerx from 'express-promise-router';
 import ActivosFijosCont from '../controllers/ActivosFijosCon';
 
-const router=routerx();
+const router = routerx();
 
-router.post('/guardar',ActivosFijosCont.Guardar);
-router.get('/list',ActivosFijosCont.list);
-router.post('/buscarPorId',ActivosFijosCont.buscarPorId);
-router.post('/BuscarAFxEmpresa',ActivosFijosCont.BuscarAFxEmpresa);
-router.put('/update',ActivosFijosCont.update);
-router.delete('/remove',ActivosFijosCont.remove);
-router.put('/actualizaReserva',ActivosFijosCont.ActualizarReserva);
-router.put('/procesar',ActivosFijosCont.procesar);
-router.put('/cambiar',ActivosFijosCont.cambiar);
-router.post('/mostrarProductos',ActivosFijosCont.mostrarProductos);
-router.post('/mostrarProductosweb',ActivosFijosCont.mostrarProductosweb);
-router.post('/comprobarCodigo',ActivosFijosCont.comprobarCodigo);
-router.get('/obtenerDetalle',ActivosFijosCont.listarActivos);
-router.post('/listarXBodega',ActivosFijosCont.listarActivosXBodega);
-router.post('/actualizarBodegas',ActivosFijosCont.actualizarBodegas);
+router.post('/guardar', ActivosFijosCont.Guardar);
+router.get('/list', ActivosFijosCont.list);
+router.post('/buscarPorId', ActivosFijosCont.buscarPorId);
+router.post('/BuscarAFxEmpresa', ActivosFijosCont.BuscarAFxEmpresa);
+router.put('/update', ActivosFijosCont.update);
+router.delete('/remove', ActivosFijosCont.remove);
+router.put('/actualizaReserva', ActivosFijosCont.ActualizarReserva);
+router.put('/procesar', ActivosFijosCont.procesar);
+router.put('/cambiar', ActivosFijosCont.cambiar);
+router.post('/mostrarProductos', ActivosFijosCont.mostrarProductos);
+router.post('/mostrarProductosweb', ActivosFijosCont.mostrarProductosweb);
+router.post('/comprobarCodigo', ActivosFijosCont.comprobarCodigo);
+router.get('/obtenerDetalle', ActivosFijosCont.listarActivos);
+router.post('/listarXBodega', ActivosFijosCont.listarActivosXBodega);
+router.post('/actualizarBodegas', ActivosFijosCont.actualizarBodegas);
 
 
 
--- routes/BodegaRo.js
+++ routes/BodegaRo.js
@@ -1,17 +1,17 @@
 import routerx from 'express-promise-router';
 import BodegaCont from '../controllers/BodegaCon';
 
-const router=routerx();
+const router = routerx();
 
-router.post('/guardar',BodegaCont.Guardar);
-router.get('/query',BodegaCont.query);
-router.get('/list',BodegaCont.list);
-router.put('/update',BodegaCont.update);
-router.delete('/remove',BodegaCont.remove);
-router.put('/activate',BodegaCont.activate);
-router.put('/deactivate',BodegaCont.deactivate);
+router.post('/guardar', BodegaCont.Guardar);
+router.get('/query', BodegaCont.query);
+router.get('/list', BodegaCont.list);
+router.put('/update', BodegaCont.update);
+router.delete('/remove', BodegaCont.remove);
+router.put('/activate', BodegaCont.activate);
+router.put('/deactivate', BodegaCont.deactivate);
 router.post('/BuscarBodegaxEmpresa', BodegaCont.BuscarBodegaxEmpresa);
-router.put('/cambiar',BodegaCont.cambiar);
+router.put('/cambiar', BodegaCont.cambiar);
 router.post('/mostrarBodegas', BodegaCont.mostrarBodegas);
 router.get('/bodegas', BodegaCont.bodegas);
 
